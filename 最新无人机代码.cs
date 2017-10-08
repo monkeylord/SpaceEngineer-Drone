fa1222f99e228cd578afdd45a2af671fe04deddc
monkeylord <bluegod_001@126.com>
2017-10-08 20:31:48 +0800

    update

--- 最新无人机代码.cs
+++ 最新无人机代码.cs
@@ -1,3 +1,316 @@
+List<IMyTerminalBlock> list = new List<IMyTerminalBlock>();     
+List<IMyTerminalBlock> listT = new List<IMyTerminalBlock>();     
+   
+//Test Small Drone 0x03   
+//Base model   
+   
+//Settings:   
+//Patrol Range (meter)	巡逻半径   
+int Range = 30000;   
+//Navigation		导航及规避   
+Vector3D origin = new Vector3D(0, 0, 0);     
+Vector3D target = new Vector3D(0, 0, 0);     
+   
+//距离设定   
+int DstDodge=1000;   
+int DstFire=800;   
+int DstSafe=800;   
+int DstChaseFireRange=1200; 
+int MaxAttackDst = 700;   
+int MinAttackDst = 600;   
+ 
+   
+int Tick=1;   
+int debugcount=0;   
+UnionControl uc=null; 
+void Main(string argument)     
+{     
+	try{   
+		if(!PilotCheck()){Echo("MalFunctional");return;}//基本自检   
+		if(uc==null){ 
+			uc=new UnionControl(this,remote); 
+			uc.Manual(true); 
+		}else uc.Update(); 
+		if(!TargetAcquire(argument))stgNow=Strategys.Return;//获取目标   
+		WeaponCheck();//武器状态检查   
+		handleStg(stgNow);//开始执行策略   
+		if(LastAquired>300){   
+			GridTerminalSystem.GetBlocksOfType<IMyRadioAntenna>(list);   
+			if(list.Count>0)list[0].SetCustomName("Target Lost");   
+		}   
+		if(list.Count>1)list[1].SetCustomName(debugcount.ToString());   
+		debugcount++;   
+	}catch(Exception e){   
+		GridTerminalSystem.GetBlocksOfType<IMyRadioAntenna>(list);   
+		if(list.Count>0)list[0].SetCustomName(e.ToString());   
+	}   
+}   
+   
+//检查开始   
+   
+//武器检查   
+bool MannualWeapon=true; 
+bool AutoWeapon=false; 
+List<IMyUserControllableGun> ManualGun=new List<IMyUserControllableGun>(); 
+private bool WeaponCheck()   
+{   
+	//TODO   
+	if(ManualGun.Count==0)GridTerminalSystem.GetBlocksOfType<IMyUserControllableGun>(ManualGun);      
+	MannualWeapon=true;   
+	AutoWeapon=false;   
+	return true;   
+} 
+ 
+//基本自检   
+IMyRemoteControl remote = null;   
+IMyTerminalBlock timer = null;   
+Vector3D MyPos = new Vector3D(0, 0, 0);     
+Vector3D MySpeed = new Vector3D(0, 0, 0);     
+private bool PilotCheck()   
+{   
+	if(remote==null||remote.IsFunctional==false){   
+		remote=null;   
+		GridTerminalSystem.GetBlocksOfType<IMyRemoteControl>(list);     
+		for(int i=0;i<list.Count;i++)     
+		{     
+			if(list[i].IsFunctional){   
+				remote = list[i] as IMyRemoteControl;    
+				break;   
+			}   
+		}   
+		if(remote==null){   
+			Echo("RemoteControl Not Found");   
+			return false;   
+		};   
+	}   
+	MySpeed=remote.GetShipVelocities().LinearVelocity;   
+	MyPos=remote.GetPosition();   
+	   
+	if(timer==null||timer.IsFunctional==false){   
+		timer=null;   
+		GridTerminalSystem.GetBlocksOfType<IMyTimerBlock>(list);     
+		for(int i=0;i<list.Count;i++)     
+		{     
+			if(list[i].IsFunctional){   
+				timer = list[i] as IMyTimerBlock;    
+				break;   
+			}   
+		}   
+		if(timer==null){   
+			Echo("Timer Not Found");   
+			return false;   
+		};   
+	}   
+	Tick=1;   
+	return true;   
+}   
+   
+//目标获取   
+bool Engage=false;   
+Vector3D Target = new Vector3D(0, 0, 0);     
+Vector3D TargetSpeedPerTick = new Vector3D(0, 0, 0);   
+Vector3D TargetAcceleration = new Vector3D(0, 0, 0); 
+double TargetDistance=0;   
+float TargetAngle=0;   
+int LastAquired=0;   
+private bool TargetAcquire(string target){   
+	Vector3D pos;   
+	Engage=false;   
+	/* 
+	GridTerminalSystem.GetBlocksOfType<IMyRadioAntenna>(list);   
+	if(list.Count>0)Vector3D.TryParse(target, out pos);     
+	else */	remote.GetNearestPlayer(out pos);   
+	   
+	if(pos!=new Vector3D(0,0,0)&&pos!=null){   
+		LastAquired=0;   
+		TargetAcceleration=((pos-Target)/Tick-TargetSpeedPerTick)/Tick*60; 
+		TargetSpeedPerTick=(pos-Target)/Tick;   
+		Target=pos;   
+		TargetDistance=Vector3D.Distance(Target, MyPos);   
+	}   
+	else LastAquired+=Tick;   
+	   
+	if(LastAquired>600)return false;   
+	TargetDistance=Vector3D.Distance(Target, MyPos);   
+	//TODO   
+	TargetAngle=0;   
+	   
+	   
+	Echo(TargetDistance.ToString());   
+	Engage=true;   
+	return Engage;   
+}   
+   
+//态势感知结束   
+   
+//策略部分开始   
+   
+//策略   
+enum Strategys { Stasis, Return, QuickApproach, AimAndShoot, Dodge, Chase, Dock, Suicide };   
+Strategys stgNow=Strategys.Stasis;   
+Strategys stgLast=Strategys.Stasis;   
+bool State_BeingChased=false; 
+bool State_Chasing=false; 
+bool State_Disengage=false; 
+//总控   
+void handleStg(Strategys stg){   
+	stgLast=stgNow; 
+	StateCheck(); 
+	if(State_Disengage)stgNow=Strategys.QuickApproach; 
+	else if(State_BeingChased)stgNow=Strategys.Dodge; 
+	else stgNow=stg;   
+	switch(stgNow){   
+		case Strategys.Stasis:   
+			Strategy_Stasis();   
+			break;   
+		case Strategys.Return:   
+			Strategy_ReturnOrigin();   
+			break;   
+		case Strategys.QuickApproach:   
+			Strategy_QuickApproach();   
+			break;   
+		case Strategys.AimAndShoot:   
+			Strategy_AimAndShoot();   
+			break;    
+		case Strategys.Dodge:   
+			Strategy_Dodge();   
+			break; 
+		case Strategys.Chase: 
+			Strategy_Chase(); 
+			break;   
+		case Strategys.Dock:   
+			break;   
+		case Strategys.Suicide:   
+			break;   
+	} 
+	Echo(stgNow.ToString()+"Done"); 
+	//remote.SetAutoPilotEnabled(true);       
+}   
+   
+void StateCheck(){   
+	//定义各类情况   
+	State_BeingChased=((TargetSpeedPerTick*60).Length()>30&&Vector3D.Dot(Vector3D.Normalize(TargetSpeedPerTick),Vector3D.Normalize(MyPos-Target))>0.85&&TargetDistance < DstSafe*2)?true:false; 
+	State_Chasing=(Vector3D.Dot(TargetSpeedPerTick*0.6,MySpeed)>80)?true:false; 
+	State_Disengage=(TargetDistance > DstSafe * 3 && LastAquired<300)?true:false; 
+}   
+   
+private void Strategy_ReturnOrigin(){   
+	uc.Output(100f); 
+	uc.Heading(origin);     
+	//5 km 安全休眠距离   
+	if (Vector3D.Distance(Target, remote.GetPosition ()) > 5000)handleStg(Strategys.Stasis);   
+}   
+   
+private void Strategy_Stasis(){   
+	if(Engage){uc.Inertia(false);uc.Output(0f);handleStg(Strategys.QuickApproach);}   
+	else uc.Inertia(true);   
+}   
+   
+Vector3D dodge;   
+private void Strategy_Chase(){   
+	if(TargetDistance>=DstSafe*2){   
+		stgNow=Strategys.QuickApproach;  
+	}else{   
+		Vector3D TargetFixed=HitPointCaculateV3(MyPos, MySpeed, new Vector3D(0), Target, TargetSpeedPerTick*60, TargetAcceleration*60, 400, 1, 1.55, false); 
+		uc.Heading(TargetFixed);  
+		Random ran=new Random(); 
+		if(debugcount%60==0)sign=(ran.Next(-50,50)<0)?sign*-1:sign; 
+		uc.Output(Vector3D.Normalize(Vector3D.Cross(MySpeed,MyPos-Target)*sign)*85+Vector3D.Normalize(RandomMove())*15); 
+	}  
+}   
+private void Strategy_QuickApproach(){   
+	if(TargetDistance<DstDodge){   
+		handleStg(Strategys.AimAndShoot);   
+	}else{   
+		uc.Output(100f);   
+		uc.Heading(Target+TargetSpeedPerTick*60);   
+	}   
+}   
+ 
+int sign=1; 
+private void Strategy_AimAndShoot(){   
+	Echo("Attacking");   
+	if(remote.RotationIndicator.Length()<0.05 && TargetDistance<DstFire+Vector3D.Dot(TargetSpeedPerTick,Vector3D.Normalize(MySpeed))*120)ManualShot();   
+	Vector3D TargetFixed=HitPointCaculateV3(MyPos, MySpeed, new Vector3D(0), Target, TargetSpeedPerTick*60, TargetAcceleration*60, 400, 1, 1.55, false); 
+	uc.Heading(TargetFixed);    
+	//Vector3D DstKeepV=(TargetDistance<MinAttackDst||TargetDistance>MaxAttackDst)?Vector3D.Normalize(MyPos-Target)*(TargetDistance-(MinAttackDst+MaxAttackDst)/2):new Vector3D(0,0,0); 
+	Vector3D DstKeepV=Vector3D.Normalize(MyPos-Target)*Math.Min(Math.Max((TargetDistance-(MinAttackDst+MaxAttackDst)/2),-200),200);
+	Random ran=new Random(); 
+	if(debugcount%60==0)sign=(ran.Next(-50,50)<0)?sign*-1:sign; 
+	uc.Output(((State_Chasing)?Vector3D.Zero:DstKeepV)+Vector3D.Normalize(Vector3D.Cross(MySpeed,MyPos-Target)*sign)*50+Vector3D.Normalize(RandomMove())*15); 
+}  
+   
+   
+//紧急闪避   
+Vector3D edodge=new Vector3D(0,0,0);   
+private void Strategy_Dodge(){   
+	if(TargetSpeedPerTick.Length()<1||TargetDistance>DstSafe*2){   
+		stgNow=Strategys.AimAndShoot;   
+	} 
+	Vector3D MyTargetAcc=(TargetAcceleration.Length()<1)?new Vector3D(1,0,0):TargetAcceleration; 
+	edodge = Vector3D.Normalize(Vector3D.Cross(TargetSpeedPerTick,MyTargetAcc))*80; 
+	uc.Output(MyPos+edodge+RandomMove()*20); 
+	uc.Heading(HitPointCaculateV3(MyPos, MySpeed, new Vector3D(0), Target, TargetSpeedPerTick*60, TargetAcceleration*60, 400, 1, 1.55, false)); 
+	if(TargetDistance<DstChaseFireRange)ManualShot(); 
+}   
+//策略部分结束   
+   
+   
+//功能部分开始   
+ 
+//旧算法v3.0 
+Vector3D HitPointCaculateV3(Vector3D Me_Position, Vector3D Me_Velocity, Vector3D Me_Acceleration, Vector3D Target_Position, Vector3D Target_Velocity, Vector3D Target_Acceleration, double Bullet_Speed, double Velocity_Factor, double Acceleration_Factor, bool AbsolutelyBullet) 
+{ 
+	double HitTime = Vector3D.Distance(Me_Position, Target_Position)/Bullet_Speed; 
+	 
+	Vector3D Me_Velocity_Effect = new Vector3D(); 
+	if(AbsolutelyBullet) 
+	{ 
+		Me_Velocity_Effect = new Vector3D(0,0,0); 
+	} 
+	else 
+	{ 
+		Me_Velocity_Effect = Me_Velocity; 
+	} 
+	 
+	Vector3D HitPosition = Target_Position + (Target_Velocity+(0.5*Target_Acceleration*HitTime))*HitTime - Me_Velocity_Effect*HitTime; 
+	 
+	HitTime = Vector3D.Distance(HitPosition, Me_Position)/Bullet_Speed; 
+	HitPosition = Target_Position + (Target_Velocity*Velocity_Factor+(0.5*Target_Acceleration*HitTime*Acceleration_Factor))*HitTime - Me_Velocity_Effect*HitTime*Velocity_Factor; 
+	 
+	//多次逼近修正 
+	HitTime = Vector3D.Distance(HitPosition, Me_Position)/Bullet_Speed; 
+	HitPosition = Target_Position + (Target_Velocity*Velocity_Factor+(0.5*Target_Acceleration*HitTime*Acceleration_Factor))*HitTime - Me_Velocity_Effect*HitTime*Velocity_Factor; 
+	HitTime = Vector3D.Distance(HitPosition, Me_Position)/Bullet_Speed; 
+	HitPosition = Target_Position + (Target_Velocity*Velocity_Factor+(0.5*Target_Acceleration*HitTime*Acceleration_Factor))*HitTime - Me_Velocity_Effect*HitTime*Velocity_Factor; 
+	HitTime = Vector3D.Distance(HitPosition, Me_Position)/Bullet_Speed; 
+	HitPosition = Target_Position + (Target_Velocity*Velocity_Factor+(0.5*Target_Acceleration*HitTime*Acceleration_Factor))*HitTime - Me_Velocity_Effect*HitTime*Velocity_Factor; 
+	HitTime = Vector3D.Distance(HitPosition, Me_Position)/Bullet_Speed; 
+	HitPosition = Target_Position + (Target_Velocity*Velocity_Factor+(0.5*Target_Acceleration*HitTime*Acceleration_Factor))*HitTime - Me_Velocity_Effect*HitTime*Velocity_Factor; 
+	HitTime = Vector3D.Distance(HitPosition, Me_Position)/Bullet_Speed; 
+	HitPosition = Target_Position + (Target_Velocity*Velocity_Factor+(0.5*Target_Acceleration*HitTime*Acceleration_Factor))*HitTime - Me_Velocity_Effect*HitTime*Velocity_Factor; 
+	 
+	return HitPosition; 
+}
+//闪避算法   
+Vector3D r60=new Vector3D(0,0,0); 
+Vector3D r30=new Vector3D(0,0,0); 
+Vector3D r10=new Vector3D(0,0,0); 
+private Vector3D RandomMove(){ 
+	Random ran=new Random();  
+	if(debugcount%61==0)r60=new Vector3D(ran.Next(-450,450),ran.Next(-450,450), ran.Next(-450,450)); 
+	if(debugcount%31==0)r30=new Vector3D(ran.Next(-450,450),ran.Next(-450,450), ran.Next(-450,450)); 
+	if(debugcount%11==0)r10=new Vector3D(ran.Next(-450,450),ran.Next(-450,450), ran.Next(-450,450)); 
+	return r60*0.1+r30+0.2+r10*0.3+new Vector3D(ran.Next(-450,450),ran.Next(-450,450), ran.Next(-450,450))*0.4; 
+}  
+   
+//武器控制 
+private bool ManualShot()   
+{ 
+	IMyUserControllableGun GG; 
+	foreach(IMyUserControllableGun manualGun in ManualGun)manualGun.GetActionWithName("ShootOnce").Apply(manualGun);   
+	return true;   
+} 
 public class UnionControl{
 	
 	MyGridProgram MGP;
